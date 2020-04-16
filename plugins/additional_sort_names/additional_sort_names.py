e6df49d6751841fbbbf35e9551306b8826d2e826
David Kellner <52860029+kellnerd@users.noreply.github.com>
2020-04-16 17:11:44 +0200

    sort_names: Clean up code & Add license metadata

--- plugins/additional_sort_names/additional_sort_names.py
+++ plugins/additional_sort_names/additional_sort_names.py
@@ -1,29 +1,43 @@
-PLUGIN_NAME = "Sort Names"
-PLUGIN_AUTHOR = "David Kellner"
-PLUGIN_DESCRIPTION = """Provides the artist sort names for writers and lyricists. Values are available as <code>_writersort</code> and <code>_lyricistsort</code>, e.g. for usage in a script like this one:
-<pre>
-$copymerge(composer,writer)
-$copymerge(composersort,_writersort)
-$unset(writer)
-</pre>"""
-PLUGIN_VERSION = "0.1"
-PLUGIN_API_VERSIONS = ["2.0"]
+PLUGIN_NAME = 'Additional Sort Names'
+PLUGIN_AUTHOR = 'David Kellner'
+PLUGIN_VERSION = '1.0'
+PLUGIN_API_VERSIONS = ['2.1', '2.3']
+PLUGIN_LICENSE = 'GPL-2.0-or-later'
+PLUGIN_LICENSE_URL = 'https://www.gnu.org/licenses/gpl-2.0.html'
+PLUGIN_DESCRIPTION = '''
+Provides the artist sort names for writers and lyricists.
+Values are available as additional variables <code>_writersort</code> and
+<code>_lyricistsort</code> for usage in a script.
+'''
 
+
+from picard import log
 from picard.metadata import register_track_metadata_processor
 
 
-def provide_sortnames(album, metadata, track, release):
-    writersort = []
-    lyricistsort = []
-    for trackrel in track["recording"]["relations"]:
-        if trackrel["type"] == "performance":
-            for workrel in trackrel["work"]["relations"]:
-                if workrel["type"] == "writer":
-                    writersort.append(workrel["artist"]["sort-name"])
-                elif workrel["type"] == "lyricist":
-                    lyricistsort.append(workrel["artist"]["sort-name"])
-    metadata["~writersort"] = writersort
-    metadata["~lyricistsort"] = lyricistsort
+def provide_sort_names(album, metadata, track, release):
+    recording = track['recording']
+    if not 'relations' in recording:
+        log.error('Please enable track relationships in the metadata options.')
+        return
+    writer_sort = []
+    lyricist_sort = []
+    for recording_rel in recording['relations']:
+        if recording_rel['type'] == 'performance':
+            work = recording_rel['work']
+            if not 'relations' in work:
+                continue
+            for work_rel in work['relations']:
+                rel_type = work_rel['type']
+                if not rel_type in ['writer', 'lyricist']:
+                    continue
+                artist = work_rel['artist']
+                if rel_type == 'writer':
+                    writer_sort.append(artist['sort-name'])
+                elif rel_type == 'lyricist':
+                    lyricist_sort.append(artist['sort-name'])
+    metadata['~writersort'] = writer_sort
+    metadata['~lyricistsort'] = lyricist_sort
 
 
-register_track_metadata_processor(provide_sortnames)
+register_track_metadata_processor(provide_sort_names)
